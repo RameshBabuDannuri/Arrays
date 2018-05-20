aa139b6129544509b25233d95ff56d31f8f8adae
ramesh <ramesh.dannuri@gmail.com>
2018-05-20 19:48:35 +0530

    convert array into zig zag fashion

--- src/date20_05_2018/ConvertZigZagFashion.java
+++ src/date20_05_2018/ConvertZigZagFashion.java
@@ -1,4 +1,42 @@
 package date20_05_2018;
 
+import java.util.Arrays;
+
 public class ConvertZigZagFashion {
+
+    public void sampleApproch(int a[], int n){
+        Arrays.sort(a);
+
+        for (int i = 1 ; i<n ; i+=2){
+             int temp = a[i];
+             a[i] = a[i+1];
+             a[i+1] = temp;
+        }
+
+    }
+    public void efficientApproch(int a[], int n){
+        for (int i = 0; i<n ; i+=2){
+            if (i>0 && a[i-1]>a[i]){
+                swap(a,i,i-1);
+            }
+            if (i<a.length-1 && a[i]<a[i+1]){
+                swap(a,i,i+1);
+            }
+        }
+    }
+    private void swap(int a[] , int x, int y){
+        int temp = a[x];
+        a[x]  = a[y] ;
+        a[y] = temp;
+    }
+    public static void main(String args[]){
+        ConvertZigZagFashion object = new ConvertZigZagFashion();
+        int a[]= {9,8,7,6,5,4,3,2,1};
+        //object.sampleApproch(a,a.length);
+        object.efficientApproch(a,a.length);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ,");
+        }
+    }
+
 }
--- src/date20_05_2018/FindMaxSum.java
+++ src/date20_05_2018/FindMaxSum.java
@@ -32,17 +32,18 @@ public class FindMaxSum {
 
         }
         while (i < m)
-            sum1 += a[i++];
+            sum1 = sum1+a[i++];
 
         while (j < n)
-            sum2 += b[j++];
+            sum2 = sum2+b[j++];
 
         if (sum1>sum2){
-            result += sum1;
+            result = result+ sum1;
         }
         else {
-            result+=sum2;
+            result=result+sum2;
         }
+        System.out.println(result);
 
         return result;
     }
