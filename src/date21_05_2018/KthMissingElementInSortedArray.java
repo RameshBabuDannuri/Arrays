dd743552e69c0f7e3d5314fcda492330c56d8a5f
ramesh <ramesh.dannuri@gmail.com>
2018-05-22 23:26:36 +0530

    commit

--- src/date21_05_2018/KthMissingElementInSortedArray.java
+++ src/date21_05_2018/KthMissingElementInSortedArray.java
@@ -19,8 +19,38 @@ public class KthMissingElementInSortedArray {
         }
         return missing[k-1];
     }
+    static int bestApproch(int a[] , int x){
+       int count = x;
+       int difference = 0;
+       int ans = 0;
+       boolean flag = false;
+       for (int  i =0; i<a.length ; i++){
+           difference = 0;
+           if (a[i]+1 != a[i+1]){
+               difference = difference+(a[i+1]-a[i])-1;
+             if (difference>=count){
+                 ans = a[i]+count;
+                 flag = true;
+                 break;
+             }
+             else {
+                 count = count-difference;
+             }
+
+           }
+
+       }
+       if (flag == true){
+           return ans;
+       }
+       else {
+           return -1;
+       }
+    }
     public static void main(String args[]){
         int a[]={1,3,5,7,9};
         System.out.println(find(a,2));
+        System.out.println(bestApproch(a,2));
+
     }
 }
---
+++ src/date22_05_2018/KthFrequentElement.java
@@ -0,0 +1,29 @@
+package date22_05_2018;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KthFrequentElement {
+    void find(int a[] ,  int k){
+        int n = a.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0 ; i<n ;i++){
+            if (map.containsKey(a[i])){
+                map.put(a[i] , map.get(a[i])+1);
+            }
+            else {
+                map.put(a[i],1);
+            }
+        }
+        for(Map.Entry<Integer, Integer> e :map.entrySet()){
+            if (k == e.getValue()){
+                System.out.println(e.getKey()+"  => "+e.getValue());
+            }
+        }
+    }
+    public static void main(String args[]){
+        int a[] = {10,20,30,20,10,20,20,50,70,70};
+        KthFrequentElement obj = new KthFrequentElement();
+        obj.find(a,2);
+    }
+}
