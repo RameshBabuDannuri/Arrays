38da1ca2f31ff687106995fcd83cb7cd87da7981
ramesh <ramesh.dannuri@gmail.com>
2018-05-31 23:08:51 +0530

    binary tree

--- src/date_30_05_2018/FindDistance.java
+++ src/date_30_05_2018/FindDistance.java
@@ -2,18 +2,6 @@ package date_30_05_2018;
 
 public class FindDistance {
 
-    static class Node{
-        int data;
-        Node left ;
-        Node right;
-        Node(int data){
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
-    }
-     Node root;
-
     int findDistance(Node root , int n1, int n2){
 
         Node lca = findLCA(root , n1,n2);
@@ -23,7 +11,7 @@ public class FindDistance {
 
         return d1+d2;
     }
-    static Node findLCA(Node node, int n1, int n2){
+     Node findLCA(Node node, int n1, int n2){
 
         if (node == null){
             return null;
@@ -32,12 +20,17 @@ public class FindDistance {
             return node;
         }
         Node lLca = findLCA(node.left , n1,n2);
-        Node rLca = findLCA(node.right , n2,n2);
+        Node rLca = findLCA(node.right , n1,n2);
 
         if (lLca!=null && rLca!=null){
             return node;
         }
-        return lLca!=null? lLca : rLca;
+        if (lLca!=null){
+            return lLca;
+        }
+        else {
+            return rLca;
+        }
     }
     int findLevel(Node node1 , int n, int level){
 
@@ -57,6 +50,8 @@ public class FindDistance {
     }
     public static void main(String[] args) {
 
+        FindDistance tree = new FindDistance();
+
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
@@ -66,14 +61,18 @@ public class FindDistance {
         root.right.right = new Node(7);
         root.right.left.right = new Node(8);
 
-        FindDistance tree = new FindDistance();
-        System.out.println("Dist(4, 5) = "
-                + tree.findDistance(root, 4, 5));
+
+      System.out.println("Dist(3,4) = "
+              + tree.findDistance(root, 3, 7));
 
         System.out.println("Dist(4, 6) = "
                 + tree.findDistance(root, 4, 6));
+        System.out.println("Dist(4, 7) = "
+                + tree.findDistance(root, 4, 7));
+        System.out.println("Dist(2, 8) = "
+                + tree.findDistance(root, 2, 8));
 
-        System.out.println("Dist(3, 7) = "
-                + tree.findDistance(root, 3, 4));
+
+
     }
 }
